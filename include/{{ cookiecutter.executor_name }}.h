320ee8eb34d0d452319db3ed02ef91c40700454a
LazyPanda07 <semengricenko@gmail.com>
2025-12-17 23:34:57 +0500

    Add base structure

---
+++ include/{{ cookiecutter.executor_name }}.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <Executors/{{ cookiecutter.executor_type }}.hpp>
+
+class { { cookiecutter.executor_name } } : public framework::{{ cookiecutter.executor_type }}
+{
+public:
+	void init(const framework::utility::ExecutorSettings & settings) override;
+
+	void doPost(framework::HttpRequest & request, framework::HttpResponse & response) override;
+
+	void doGet(framework::HttpRequest & request, framework::HttpResponse & response) override;
+
+	void doHead(framework::HttpRequest & request, framework::HttpResponse & response) override;
+
+	void doPut(framework::HttpRequest & request, framework::HttpResponse & response) override;
+
+	void doDelete(framework::HttpRequest & request, framework::HttpResponse & response) override;
+
+	void doPatch(framework::HttpRequest & request, framework::HttpResponse & response) override;
+};
