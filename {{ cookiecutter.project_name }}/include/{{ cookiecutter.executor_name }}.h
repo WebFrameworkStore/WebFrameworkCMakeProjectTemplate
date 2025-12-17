56f37689cef9dfdd8356fc0e5f1ec9e503847b05
LazyPanda07 <semengricenko@gmail.com>
2025-12-17 23:56:40 +0500

    Refactor

--- {{ cookiecutter.project_name }}/include/{{ cookiecutter.executor_name }}.h
+++ {{ cookiecutter.project_name }}/include/{{ cookiecutter.executor_name }}.h
@@ -2,7 +2,7 @@
 
 #include <Executors/{{ cookiecutter.executor_type }}.hpp>
 
-class { { cookiecutter.executor_name } } : public framework::{{ cookiecutter.executor_type }}
+class {{ cookiecutter.executor_name }} : public framework::{{ cookiecutter.executor_type }}
 {
 public:
 	void init(const framework::utility::ExecutorSettings & settings) override;
